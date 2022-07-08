1d9643ff9481dcf1425653baf1db868b1eea2ef6
Torikul Islam <toriqulislam142@gmail.com>
2022-07-08 11:09:15 -0500

    some modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Header from './components/Header/Header';
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <div className="App">
+      <h1 className='text-4xl'>Hello From Github Practic Project</h1>
+      <h2>Components Added</h2>
+    </div>
   );
 }
 
