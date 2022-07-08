41a4d0cacad08149925b581b8e4ec4bfec0b3387
Mazharul islam <mazharulislam.dev@gmail.com>
2022-07-08 21:07:07 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-4xl'>Hello From Github Practic Project</h1>
-      <h2>Components Added</h2>
+      <Home />
     </div>
   );
 }
