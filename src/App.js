b0ab9ae7be44cdad1cbfa6fbf6a69f64a38175d7
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-07-08 20:38:55 +0600

    changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import ClientsSay from './components/Clients';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-4xl'>Hello From Github Practic Project</h1>
       <h2>Components Added</h2>
+      <ClientsSay></ClientsSay>
     </div>
   );
 }
---
+++ src/components/Clients.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ClientsSay = () => {
+  return (
+    <div>
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content text-center">
+          <div class=" ">
+            <h1 class="text-5xl font-bold grid grid-flow-row">What Our Clients Say
+            </h1>
+             <h1 className=" items-center justify-center flex p-5 ">
+             <img class="mask mask-circle" src="https://i.ibb.co/VNgXWx4/Ellipse-2.png" alt="Clients pic"/>
+             </h1>
+            <p class="p-4 text-3xl">
+           
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit eos inventore, adipisci deleniti numquam accusamus facere cumque sequi magnam earum ducimus necessitatibus perspiciatis repellat esse sint. 
+            </p>
+            <p className="text-2xl font-bold">Rihanat Oluwadamilola Orire</p>
+            <p className="font-bold">RoyBil Ltd.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClientsSay;
