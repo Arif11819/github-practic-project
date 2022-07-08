601782e53ef974c5245b865405d1609889d36266
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-07-08 22:34:25 +0600

    deleted card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ClientsSay from './components/Clients';
+import ClientsSay from './components/Clients/Clients';
 
 function App() {
   return (
@@ -7,6 +7,7 @@ function App() {
       <h1 className='text-4xl'>Hello From Github Practic Project</h1>
       <h2>Components Added</h2>
       <ClientsSay></ClientsSay>
+      
     </div>
   );
 }
