174c9b48e0be3b88fa5e08e64039ba27d0336bd1
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-07-08 22:54:53 +0600

    pull

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import ClientsSay from './components/Clients/Clients';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-4xl'>Hello From Github Practic Project</h1>
-      <h2>Components Added</h2>
-      <ClientsSay></ClientsSay>
-      
+
     </div>
   );
 }
